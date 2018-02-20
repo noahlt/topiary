49dbda0084acf1102d6ac40b7330e46a08f6565e
Noah Tye <hi@noahtye.com>
2018-02-19 19:57:11 -0800

    add websockets; players can join and see player list

--- client/static/client.js
+++ client/static/client.js
@@ -1,10 +1,85 @@
-window.onload = function() {
-  fetch('/api/connect').then((response) => {
+var MODE_SETUP = 0;
+var MODE_CONNECTING = 1;
+var MODE_PLAYING = 2;
+
+var CLIENT_MODE = MODE_SETUP;
+var WS = null;
+
+function render() {
+  console.log('render');
+  if (CLIENT_MODE === MODE_SETUP) {
+    renderSetup();
+  } else if (CLIENT_MODE === MODE_CONNECTING) {
+    renderConnecting();
+  } else if (CLIENT_MODE === MODE_PLAYING) {
+    renderPlaying();
+  }
+}
+
+function renderSetup() {
+  console.log('renderSetup');
+  document.body.innerHTML = '<div>Player Name: <input id="name" type="text" /></div>' +
+    '<div>Player Letter: <input id="letter" type="text" /></div>' +
+    '<div><button id="join">join game</button>';
+
+  document.getElementById('join').addEventListener('click', (event) => {
+    var name = document.getElementById('name').value;
+    var letter = document.getElementById('letter').value;
+    if (letter.length !== 1) {
+      alert('sorry, you can only have one letter to for your letter');
+      return;
+    }
+    modeConnect(name, letter);
+  });
+}
+
+function modeSetup() {
+  CLIENT_MODE = MODE_SETUP;
+  WS = null;
+  renderSetup();
+}
+
+function modeConnect(name, letter) {
+  CLIENT_MODE = MODE_CONNECTING;
+  render();
+
+  fetch('/api/connect', {
+    method: 'POST',
+    body: JSON.stringify({name: name, letter: letter}),
+    headers: new Headers({'Content-Type': 'application/json'}),
+  }).then((response) => {
     return response.json();
   }).then((data) => {
-    console.log(data);
-    document.body.innerHTML = data.message;
+    console.log('api/connect response:', data);
+    modePlay();
   }).catch((error) => {
     console.error('error!', error);
   });
-};
+}
+
+function modePlay(playerID) {
+  CLIENT_MODE = MODE_PLAYING;
+  WS = new WebSocket('ws://' + location.hostname + ':8001');
+  WS.onopen = () => {
+    console.log('ws open');
+    WS.onmessage = (mesg) => {
+      var data = JSON.parse(mesg.data);
+      console.log('received message', data);
+      if (data.type === 'playerList') {
+        renderPlaying(data.playerList);
+      }
+    }
+    WS.send(JSON.stringify({type: 'requestPlayerList'}));
+  }
+}
+
+function renderPlaying(playerList) {
+  var labels = playerList.map((p) => `<div>${p.letter} &mdash; ${p.name}</div>`);
+  document.body.innerHTML = labels.join('\n');
+}
+
+function renderConnecting() {
+  document.body.innerHTML = 'connecting...';
+}
+
+window.onload = render;
--- server/server.js
+++ server/server.js
@@ -2,16 +2,100 @@ var ip = require('ip');
 var path = require('path');
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 STATIC_DIR = '../client/static';
+
+var WebSocketServer = require('ws').Server;
+var WSS = new WebSocketServer({ port: 8001 });
+/*
+class Session {
+  constructor() {
+    this.players = [];
+  }
+  
+  fullBroadcast() {
+    broadcastClients('session', selectKeys(this, ['players']));
+  }
+ 
+  addPlayer(name, letter) {
+    this.players.push({name: name, letter: letter});
+    fullBroadcast();
+  }
+}
+
+var SESSION = new Session(); // a singleton for now
+*/
+PLAYERS = [];
+addPlayer = function(name, letter) {
+  PLAYERS.push({name: name, letter: letter});
+}
+var CONNECTIONS = [];
+
+function selectKeys(x, keys) {
+  var r = {};
+  keys.forEach((k) => {
+    r[k] = x[k];
+  });
+  return r;
+}
+
+WSS.on('connection', (ws) => {
+  CONNECTIONS.push(ws);
+  console.log('added a new ws connection');
+  broadcastClients('playerList', {playerList: PLAYERS});
+
+  // ws.onmessage = (event) => {
+  //   var data = JSON.parse(event.data);
+  //   console.log('received mesg while connecting', data);
+  //   if (data.type === 'newPlayer') {
+  //     console.log('new player requests to join');
+  //     if (data.name.length > 0 && data.letter.length === 1) {
+  //       console.log('new player is valid');
+  //       players.push({name: data.name, letter: data.letter, conn: ws});
+  //       ws.send(JSON.stringify({
+  //         type: 'playerAdded',
+  //         letter: data.letter,
+  //         name: data.name,
+  //       }));
+  //       broadcastClients('playerList', {playerList: players.map(
+  //         (p) => selectKeys(p, ['name', 'letter']))});
+  //     } else {
+  //       ws.send(JSON.stringify({
+  //         type: 'playerRejected',
+  //       }));
+  //     }
+  //   } if (data.type === 'requestPlayerList') {
+  //   } else {
+  //     console.log('received other message:', data.type);
+  //   }
+  // }
+});
+
+function broadcastClients(mesgType, data) {
+  var json = JSON.stringify(Object.assign({type: mesgType}, data));
+  console.log('broadcasting:', json);
+  CONNECTIONS.forEach((conn) => conn.send(json));
+}
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendFile(path.normalize(path.join(__dirname, STATIC_DIR, "index.html")));
 });
 
-app.get('/api/connect', function(req, res) {
-  res.send('{"message":"welcome to the game!"}');
+app.post('/api/connect', function(req, res) {
+  if (req.body.name.length < 1 && req.body.letter.length !== 1) {
+    res.sendStatus(400);
+    return;
+  }
+  addPlayer(req.body.name, req.body.letter);
+  res.send(JSON.stringify({
+    message:"welcome to the game!",
+    name: req.body.name,
+    letter: req.body.letter,
+  }));
 });
 
 app.use('/static', express.static('../client/static'));
